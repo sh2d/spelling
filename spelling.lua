c1340919b658ad8870e1d3d0c710fa79c88fd447
Stephan Hennig <sh2d@arcor.de>
2012-11-25 20:00:07 +0100

    Replace custom Unicode code point to UTF-8 conversion function by unicode.utf8.char from Selene library.
    Note, the Selene function only covers the range U-00000000 to U-0010FFFF.

--- spelling.lua
+++ spelling.lua
@@ -18,6 +18,7 @@ fd = io.open(tex.jobname .. '.txt','wb')
 -- Function short-cuts.
 local tabconcat = table.concat
 local tabinsert = table.insert
+local utf8char = unicode.utf8.char
 local utf8len = unicode.utf8.len
 
 
@@ -26,83 +27,6 @@ local DISC = node.id('disc')
 local GLYPH = node.id('glyph')
 local KERN = node.id('kern')
 local PUNCT = node.id('punct')
-
-
---- Convert Unicode code points to UTF-8.
--- This function converts Unicode code points to UTF-8 characters.  It
--- is modelled after the following table taken from the UTF-8 and
--- Unicode FAQ for Unix/Linux found at
--- <URL:http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8>:
---
--- U-00000000 – U-0000007F:  0xxxxxxx
--- U-00000080 – U-000007FF:  110xxxxx 10xxxxxx
--- U-00000800 – U-0000FFFF:  1110xxxx 10xxxxxx 10xxxxxx
--- U-00010000 – U-001FFFFF:  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
--- U-00200000 – U-03FFFFFF:  111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
--- U-04000000 – U-7FFFFFFF:  1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
---
--- @param cp  A Unicode code point.
--- @return  A string representing a UTF-8 encoded character.
-cp2utf8 = function(cp)
-  -- range U-00000000 – U-0000007F:  0xxxxxxx
-  if cp < 0x80 then
-    local one = cp
-    return string.char(one)
-  end
-  -- range U-00000080 – U-000007FF:  110xxxxx 10xxxxxx
-  if cp < 0x0800 then
-    local two = cp % 64
-    cp = math.floor(cp / 64)
-    local one = cp
-    return string.char(128 + 64 + one) .. string.char(128 + two)
-  end
-  -- range U-00000800 – U-0000FFFF:  1110xxxx 10xxxxxx 10xxxxxx
-  if cp < 0x00010000 then
-    local three = cp % 64
-    cp = math.floor(cp / 64)
-    local two = cp % 64
-    cp = math.floor(cp / 64)
-    local one = cp
-    return string.char(128 + 64 + 32 + one) .. string.char(128 + two) .. string.char(128 + three)
-  end
-  -- range U-00010000 – U-001FFFFF:  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-  if cp < 0x00200000 then
-    local four = cp % 64
-    cp = math.floor(cp / 64)
-    local three = cp % 64
-    cp = math.floor(cp / 64)
-    local two = cp % 64
-    cp = math.floor(cp / 64)
-    local one = cp
-    return string.char(128 + 64 + 32 + 16 + one) .. string.char(128 + two) .. string.char(128 + three) .. string.char(128 + four)
-  end
-  -- range U-00200000 – U-03FFFFFF:  111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
-  if cp < 0x04000000 then
-    local five = cp % 64
-    cp = math.floor(cp / 64)
-    local four = cp % 64
-    cp = math.floor(cp / 64)
-    local three = cp % 64
-    cp = math.floor(cp / 64)
-    local two = cp % 64
-    cp = math.floor(cp / 64)
-    local one = cp
-    return string.char(128 + 64 + 32 + 16 + 8 + one) .. string.char(128 + two) .. string.char(128 + three) .. string.char(128 + four) .. string.char(128 + five)
-  end
-  -- range U-04000000 – U-7FFFFFFF:  1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
-  local six = cp % 64
-  cp = math.floor(cp / 64)
-  local five = cp % 64
-  cp = math.floor(cp / 64)
-  local four = cp % 64
-  cp = math.floor(cp / 64)
-  local three = cp % 64
-  cp = math.floor(cp / 64)
-  local two = cp % 64
-  cp = math.floor(cp / 64)
-  local one = cp
-  return string.char(128 + 64 + 32 + 16 + 8 + 4 + one) .. string.char(128 + two) .. string.char(128 + three) .. string.char(128 + four) .. string.char(128 + five) .. string.char(128 + six)
-end
 
 
 --- Scan a node list for words.
@@ -133,7 +57,7 @@ build_paragraph = function(head)
     if withinword then
       -- Store the characters of the current word.
       if n.id == GLYPH then
-        tabinsert(word, cp2utf8(n.char))
+        tabinsert(word, utf8char(n.char))
       end
       -- Search for the end of the current word.
       -- This definition of a word fails on '\LaTeX'!
